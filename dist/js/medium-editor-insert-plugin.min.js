24af8892ea5aeac69ed045cf5700a46bae7dac94
Sid Yadav <sidyadav@gmail.com>
2019-07-08 14:20:48 -0400

    Fix formatting for oembed captions

--- dist/js/medium-editor-insert-plugin.min.js
+++ dist/js/medium-editor-insert-plugin.min.js
@@ -1,5 +1,5 @@
 /*! 
- * medium-editor-insert-plugin v2.6.4 - jQuery insert plugin for MediumEditor
+ * medium-editor-insert-plugin v2.6.5 - jQuery insert plugin for MediumEditor
  *
  * http://linkesch.com/medium-editor-insert-plugin
  * 
